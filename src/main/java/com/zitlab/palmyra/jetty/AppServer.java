e6555e91cd1fcacd7ef5aa39e0eb573705ac8ad0
Raja K <ksvraja@gmail.com>
2021-10-29 20:06:52 +0530

    AppServer configs

--- src/main/java/com/zitlab/palmyra/jetty/AppServer.java
+++ src/main/java/com/zitlab/palmyra/jetty/AppServer.java
@@ -2,10 +2,10 @@ package com.zitlab.palmyra.jetty;
 
 import com.biocliq.fluwiz.server.api.PalmyraServer;
 
-public class ApiServer {
+public class AppServer {
 
 	public static void main(String[] args) throws Exception {
-		PalmyraServer.start(5050);
+		PalmyraServer.start(6060);
 	}
 
 }
